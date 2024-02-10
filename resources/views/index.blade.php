0bc223043c18b0380158706f225fc3c6f8c2f52f
XopK <dmahmutov12@gmail.com>
2024-02-10 14:44:53 +0500

    landing finall
    остался только подвал

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,14 +12,27 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
+    <style>
+        .review {
+            margin-bottom: 20px;
+            padding: 15px;
+            border-radius: 8px;
+        }
 
+        .review img {
+            max-width: 100%;
+            height: auto;
+            border-radius: 8px;
+            margin-bottom: 10px;
+        }
+    </style>
 </head>
 
 
 <body>
     <x-header></x-header>
     <div class="container">
-        <h1 class="text-center">Найденные животные</h1>
+        <h1 class="text-center">Новая жизнь</h1>
         <div id="carouselExampleCaptions" class="carousel slide">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
@@ -88,60 +101,184 @@
             </div>
         </div>
         <div class="animals-block">
-            <div class="row row-cols-1 row-cols-md-2 g-4">
-                <div class="col">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
+            <h1>Найденные животные</h1>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-2 row-cols-xl-3 g-3 mt-3">
+                <div class="col d-flex justify-content-center">
+                    <a href="#" style="text-decoration: none">
+                        <div class="card shadow border-0" style="width: 18rem;">
+                            <img src="/images/aHR0cDovL3d3dy5saXZlc2N.jpg" class="card-img-top ind-card-img"
+                                alt="aHR0cDovL3d3dy5saXZlc2N.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title">Кошка</h5>
+                                <p class="card-text"><small class="text-body-secondary">danya22</small></p>
+                                <p class="card-text">Район: Кировский р-н</p>
+                                <p class="card-text">Контактный номер: +78969640327</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-body-secondary">Добавлено 10 минут назад</small>
+                            </div>
                         </div>
-                        <div class="card-footer">
-                            <small class="text-body-secondary">Last updated 3 mins ago</small>
-                        </div>
-                    </div>
+                    </a>
                 </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in
-                                to additional content. This content is a little bit longer.</p>
+                <div class="col d-flex justify-content-center">
+                    <a href="#" style="text-decoration: none">
+                        <div class="card shadow border-0" style="width: 18rem;">
+                            <img src="/images/aHR0cDovL3d3dy5saXZlc2N.jpg" class="card-img-top ind-card-img"
+                                alt="aHR0cDovL3d3dy5saXZlc2N.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title">Кошка</h5>
+                                <p class="card-text"><small class="text-body-secondary">danya22</small></p>
+                                <p class="card-text">Район: Кировский р-н</p>
+                                <p class="card-text">Контактный номер: +78969640327</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-body-secondary">Добавлено 10 минут назад</small>
+                            </div>
                         </div>
-                        <div class="card-footer">
-                            <small class="text-body-secondary">Last updated 3 mins ago</small>
-                        </div>
-                    </div>
+                    </a>
                 </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in
-                                to additional content. This content is a little bit longer.</p>
+                <div class="col d-flex justify-content-center">
+                    <a href="#" style="text-decoration: none">
+                        <div class="card shadow border-0" style="width: 18rem;">
+                            <img src="/images/aHR0cDovL3d3dy5saXZlc2N.jpg" class="card-img-top ind-card-img"
+                                alt="aHR0cDovL3d3dy5saXZlc2N.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title">Кошка</h5>
+                                <p class="card-text"><small class="text-body-secondary">danya22</small></p>
+                                <p class="card-text">Район: Кировский р-н</p>
+                                <p class="card-text">Контактный номер: +78969640327</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-body-secondary">Добавлено 10 минут назад</small>
+                            </div>
                         </div>
-                        <div class="card-footer">
-                            <small class="text-body-secondary">Last updated 3 mins ago</small>
-                        </div>
-                    </div>
+                    </a>
                 </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in
-                                to additional content. This content is a little bit longer.</p>
+                <div class="col d-flex justify-content-center">
+                    <a href="#" style="text-decoration: none">
+                        <div class="card shadow border-0" style="width: 18rem;">
+                            <img src="/images/aHR0cDovL3d3dy5saXZlc2N.jpg" class="card-img-top ind-card-img"
+                                alt="aHR0cDovL3d3dy5saXZlc2N.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title">Кошка</h5>
+                                <p class="card-text"><small class="text-body-secondary">danya22</small></p>
+                                <p class="card-text">Район: Кировский р-н</p>
+                                <p class="card-text">Контактный номер: +78969640327</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-body-secondary">Добавлено 10 минут назад</small>
+                            </div>
                         </div>
-                        <div class="card-footer">
-                            <small class="text-body-secondary">Last updated 3 mins ago</small>
+                    </a>
+                </div>
+                <div class="col d-flex justify-content-center">
+                    <a href="#" style="text-decoration: none">
+                        <div class="card shadow border-0" style="width: 18rem;">
+                            <img src="/images/aHR0cDovL3d3dy5saXZlc2N.jpg" class="card-img-top ind-card-img"
+                                alt="aHR0cDovL3d3dy5saXZlc2N.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title">Кошка</h5>
+                                <p class="card-text"><small class="text-body-secondary">danya22</small></p>
+                                <p class="card-text">Район: Кировский р-н</p>
+                                <p class="card-text">Контактный номер: +78969640327</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-body-secondary">Добавлено 10 минут назад</small>
+                            </div>
                         </div>
-                    </div>
+                    </a>
+                </div>
+                <div class="col d-flex justify-content-center">
+                    <a href="#" style="text-decoration: none">
+                        <div class="card shadow border-0" style="width: 18rem;">
+                            <img src="/images/aHR0cDovL3d3dy5saXZlc2N.jpg" class="card-img-top ind-card-img"
+                                alt="aHR0cDovL3d3dy5saXZlc2N.jpg">
+                            <div class="card-body">
+                                <h5 class="card-title">Кошка</h5>
+                                <p class="card-text"><small class="text-body-secondary">danya22</small></p>
+                                <p class="card-text">Район: Кировский р-н</p>
+                                <p class="card-text">Контактный номер: +78969640327</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-body-secondary">Добавлено 10 минут назад</small>
+                            </div>
+                        </div>
+                    </a>
                 </div>
             </div>
+        </div>
+        <div class="search-animals mt-4">
+            <h1 class="mb-4">Форма поиска питомца</h1>
+
+            <form id="petSearchForm">
+                <div class="mb-3">
+                    <label for="district" class="form-label">Район:</label>
+                    <input type="text" class="form-control" id="district" name="district" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="animalType" class="form-label">Вид животного:</label>
+                    <select class="form-select" id="animalType" name="animalType" required>
+                        <option value="cat">Кошка</option>
+                        <option value="dog">Собака</option>
+                        <option value="squirrel">Суслик</option>
+                        <option value="hamster">Хорек</option>
+                    </select>
+                </div>
+
+                <div class="mb-3">
+                    <label for="searchText" class="form-label">Поиск по виду животного:</label>
+                    <input type="text" class="form-control" id="searchText" name="searchText">
+                </div>
+
+                <button type="button" class="btn btn-primary">Искать питомца</button>
+            </form>
+        </div>
+        <div class="review-block mt-4">
+            <h1 class="mb-4 text-center">Отзывы</h1>
+            <hr>
+            <div class="review">
+                <h3>Danya22</h3>
+                <p>Замечательное место для поиска потерянных питомцев! Благодаря этому сервису мы быстро нашли
+                    нашу собаку. Огромное спасибо!</p>
+                <p class="text-muted m-0">Дата отзыва: 10 февраля 2024</p>
+            </div>
+            <hr>
+            <div class="review">
+                <h3>Danya22</h3>
+                <p>Замечательное место для поиска потерянных питомцев! Благодаря этому сервису мы быстро нашли
+                    нашу собаку. Огромное спасибо!</p>
+                <p class="text-muted m-0">Дата отзыва: 10 февраля 2024</p>
+            </div>
+            <hr>
+            <div class="review">
+                <h3>Danya22</h3>
+                <p>Замечательное место для поиска потерянных питомцев! Благодаря этому сервису мы быстро нашли
+                    нашу собаку. Огромное спасибо!</p>
+                <p class="text-muted m-0">Дата отзыва: 10 февраля 2024</p>
+            </div>
+            <hr>
+            <div class="review">
+                <h3>Danya22</h3>
+                <p>Замечательное место для поиска потерянных питомцев! Благодаря этому сервису мы быстро нашли
+                    нашу собаку. Огромное спасибо!</p>
+                <p class="text-muted m-0">Дата отзыва: 10 февраля 2024</p>
+            </div>
+            <hr>
+            <div class="review">
+                <h3>Danya22</h3>
+                <p>Замечательное место для поиска потерянных питомцев! Благодаря этому сервису мы быстро нашли
+                    нашу собаку. Огромное спасибо!</p>
+                <p class="text-muted m-0">Дата отзыва: 10 февраля 2024</p>
+            </div>
+            <hr>
+            <div class="review">
+                <h3>Danya22</h3>
+                <p>Замечательное место для поиска потерянных питомцев! Благодаря этому сервису мы быстро нашли
+                    нашу собаку. Огромное спасибо!</p>
+                <p class="text-muted m-0">Дата отзыва: 10 февраля 2024</p>
+            </div>
+            <hr>
         </div>
     </div>
 </body>
